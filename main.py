461988fd6d81604dca45c8a21b38ecb313994dfc
meiyumi <meiyenn7@gmail.com>
2024-06-04 01:44:14 +0100

    added python-test, added book reader and word counter

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        frankenstein = f.read()
+        # print(f.read())
+        print(word_count(frankenstein))
+
+def word_count(book):
+    words = book.split()
+    return len(words)
+
+main()
