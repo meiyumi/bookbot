6d9b2b3d2a531082c11b5234813b14affe901bfa
meiyumi <meiyenn7@gmail.com>
2024-06-04 02:25:01 +0100

    created final report section in main

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    book_path = "books/frankenstein.txt"
+    with open(book_path) as f:
         frankenstein = f.read()
         # print(f.read())
-        # print(word_count(frankenstein))
-        print(character_count(frankenstein))
+        words = word_count(frankenstein)
+        # print(character_count(frankenstein))
+        
+        sorted_list = sorter(character_count(frankenstein))
+        print(f"--- Begin report of {book_path} ---")
+        print(f"{words} words found in the document")
+        print()
+
+        for a in sorted_list:
+            print(f"The '{a['char']}' character was found {a['num']} times")
+        print("--- End report ---")
+
 
 def word_count(book):
     words = book.split()
@@ -21,5 +32,20 @@ def character_count(book):
                 count[letter] = 1
             
     return count
+
+def sort_on(dict):
+    return dict["num"]
+
+def sorter(char_count):
+    char_list = []
+    for char in char_count:
+        char_dict = {
+            "char": char,
+            "num": char_count[char]
+        }
+        char_list.append(char_dict)
+    
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
 
 main()
