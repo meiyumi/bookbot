510e3162d6e40456b1fda81a7058456592ab96d5
meiyumi <meiyenn7@gmail.com>
2024-06-04 02:00:29 +0100

    added char counter

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ def main():
     with open("books/frankenstein.txt") as f:
         frankenstein = f.read()
         # print(f.read())
-        print(word_count(frankenstein))
+        # print(word_count(frankenstein))
+        print(character_count(frankenstein))
 
 def word_count(book):
     words = book.split()
     return len(words)
+
+def character_count(book):
+    count = {}
+    book_lowercase = book.lower()
+    
+    for letter in book_lowercase:
+        if letter.isalpha():
+            if letter in count:
+                count[letter] += 1
+            else:
+                count[letter] = 1
+            
+    return count
 
 main()
